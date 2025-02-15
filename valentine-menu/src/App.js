436de957cf4558e0c1e80a5841f26c03e6a82659
MattBaldivino <mattbaldivino@gmail.com>
2025-02-15 00:20:13 -0800

    revisions

--- valentine-menu/src/App.js
+++ valentine-menu/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className="header">
           <div className='table-container'>
             <div className='child-one'>
-              <h1>Dinner Menu</h1>
+              <h1>Lunch Menu</h1>
               <hr></hr>
               <p>February 15, 2025</p>
               <p>Matt + Trish</p>
@@ -24,8 +24,8 @@ function App() {
               <h2>Starter</h2> 
             </div>
             <div className='right'>
-              <h3>Yellowtail Kinilaw</h3>
-              <p>The Filipino counterpart to ceviche. Yellowtail is "cooked" in vinegar and combined with chilis, onions, 
+              <h3>Bluefin Kinilaw</h3>
+              <p>The Filipino counterpart to ceviche. Bluefin tuna is "cooked" in vinegar and combined with chilis, onions, 
               and ginger. A coconut cream sauce is added to mellow out the sharp flavors.</p>
             </div>
           </div>
@@ -50,7 +50,7 @@ function App() {
             </div>
             <div className='right'>
               <h3>Milk Tea Leche Flan</h3>
-              <p>Smoky and floral dahongpao oolong balance out the sweetness of a traditional leche flan.</p>
+              <p>Smoky and floral dahongpao oolong balances out the sweetness of a traditional leche flan.</p>
             </div>
           </div>
           <br></br>
@@ -60,7 +60,7 @@ function App() {
               <h2>Drink</h2> 
             </div>
             <div className='right'>
-              <h3>Highball</h3>
+              <h3>Calamansi Chu-Hi</h3>
               <p>Citron soju, sparkling water, and calamansi come together to create a refreshing cocktail that cuts through the rich kare-kare.</p>
             </div>
           </div>
